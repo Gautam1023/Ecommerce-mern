7091c3cd630b4ce869b4b0dd9115632455f71878
Gautam Sikka <gautamsikka2005@gmail.com>
2025-07-05 02:02:11 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+   
+   
+   
+const express = require('express');
+const app = express();
+ const port = 8005;
+
+ app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+
+ });
